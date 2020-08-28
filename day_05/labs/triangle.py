1341d0c9df7df20a5c1cf1ac4406a3d78fb8e00a
mindful-ai <58874523+mindful-ai@users.noreply.github.com>
2020-08-28 09:23:06 +0530

    Add files via upload

---
+++ day_05/labs/triangle.py
@@ -0,0 +1,31 @@
+class triangle(object):
+
+    ntriangles = 0
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def circumference(self):
+        pass
+
+    def shapedata(self):
+        pass
+
+
+
+class ext_triangle(triangle):
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        return super().__str__()
+
+    def area(self):
+        pass
+
+    def shapedata(self):
+        return super().shapedata()
